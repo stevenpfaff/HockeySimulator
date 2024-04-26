01700fbfd7be434d57632b2e8dea9b253936012b
Steven Pfaff <89651503+stevenpfaff@users.noreply.github.com>
2024-04-25 19:48:31 -0500

    Adding Class files

---
+++ team.py
@@ -0,0 +1,6 @@
+class Team:
+    def __init__(self, name, offense, defense, goalie):
+        self.name = name
+        self.offense = offense
+        self.defense = defense
+        self.goalie = goalie
