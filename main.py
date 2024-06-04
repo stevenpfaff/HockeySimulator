099f956fda2e9cc582d529eae54ea2f2dea44b3c
Steven Pfaff <89651503+stevenpfaff@users.noreply.github.com>
2024-06-04 16:25:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from season import SeasonSimulator
 
+
 if __name__ == "__main__":
-    simulator = SeasonSimulator()
-    simulator.simulate_season()
-    output_file = "playoffs.csv"
-    playoff_results = simulator.playoffs(output_file)
+    num_simulations = 1000
+    playoff_statistics = []
+
+    for i in range(num_simulations):
+        simulator = SeasonSimulator()
+        simulator.simulate_season()
+        standings_output_file = f"output/standings.csv"
+        simulator.sort_division_standings()
+        output_file = f"output/playoffs.csv"
+        playoff_results = simulator.playoffs(output_file)
+        playoff_statistics.extend(playoff_results)
+        simulator.write_cup_winners()
