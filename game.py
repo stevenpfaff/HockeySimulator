82e43bcc1b078edc957af61b3376a2b7cac7da71
Steven Pfaff <89651503+stevenpfaff@users.noreply.github.com>
2024-06-16 21:41:21 -0500

    Simplified probabilities in game

--- game.py
+++ game.py
@@ -1,208 +1,63 @@
 import random
 
 class Game(object):
-    def __init__(self, home, visitor):
+    def __init__(self, home, visitor, home_goalie=None, visitor_goalie=None):
         self.home = home
         self.visitor = visitor
+        self.home_goalie = home_goalie if home_goalie else home.starting_goalie
+        self.visitor_goalie = visitor_goalie if visitor_goalie else visitor.starting_goalie
         self.home_sog = self.__get_sog(home, visitor)
         self.visitor_sog = self.__get_sog(visitor, home)
-        self.home_goals = self.__get_goals(visitor, self.home_sog)
-        self.visitor_goals = self.__get_goals(home, self.visitor_sog)
+        self.home_goals = self.__get_goals(self.home_sog, self.home_goalie)
+        self.visitor_goals = self.__get_goals(self.visitor_sog, self.visitor_goalie)
         self.winner, self.regulation = self.__get_winner(self.home, self.visitor, self.home_goals, self.visitor_goals,
                                                          self.home_sog, self.visitor_sog)
 
     @classmethod
-    def __get_sog(self, offense, defense):
-        if offense.offense <= 20 and defense.defense <= 20:
-            return random.randint(18, 35)
+    def __get_sog(cls, offense, defense):
+        ranges = [
+            (20, 20, [(15, 35), (15, 32), (15, 30), (15, 28), (15, 25), (15, 22), (12, 20)]),
+            (30, 20, [(16, 38), (16, 35), (16, 33), (16, 31), (16, 28), (16, 25), (13, 23)]),
+            (40, 20, [(17, 41), (17, 38), (17, 36), (17, 34), (17, 31), (17, 28), (14, 26)]),
+            (50, 20, [(18, 44), (18, 41), (18, 39), (18, 37), (18, 34), (18, 31), (15, 29)]),
+            (60, 20, [(20, 47), (20, 44), (20, 42), (20, 40), (19, 37), (19, 34), (16, 32)]),
+            (70, 20, [(22, 50), (22, 47), (22, 45), (22, 43), (20, 40), (20, 37), (18, 35)]),
+            (80, 20, [(25, 53), (25, 50), (25, 48), (25, 46), (22, 43), (22, 40), (20, 38)]),
+        ]
 
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 20:
-            return random.randint(20, 35)
+        def get_random_value(offense_val, defense_val):
+            for o_max, d_max, value_ranges in ranges:
+                if offense_val <= o_max:
+                    for d_threshold, value_range in zip(range(20, 81, 10), value_ranges):
+                        if defense_val <= d_threshold:
+                            return random.randint(*value_range)
+            return 0
 
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 20:
-            return random.randint(22, 38)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 20:
-            return random.randint(25, 40)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 20:
-            return random.randint(28, 42)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 20:
-            return random.randint(28, 45)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 20:
-            return random.randint(30, 50)
-
-        # 30-21 Defense Team 2
-        elif offense.offense <= 20 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(15, 32)
-
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(18, 32)
-
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(18, 35)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(20, 35)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(25, 38)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(28, 42)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 30 & defense.defense > 20:
-            return random.randint(30, 50)
-
-        # 40-31 Defense Team 2
-        elif offense.offense <= 20 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(13, 28)
-
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(15, 30)
-
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(15, 32)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(18, 35)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(22, 38)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(25, 40)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 40 & defense.defense > 30:
-            return random.randint(28, 48)
-
-
-        # 50-41 Defense Team 2
-        elif offense.offense <= 20 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(13, 25)
-
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(13, 28)
-
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(15, 30)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(18, 32)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(22, 35)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(25, 40)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 50 & defense.defense > 40:
-            return random.randint(25, 40)
-
-
-        # 60-51 Defense Team 2
-        elif offense.offense <= 20 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(13, 22)
-
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(13, 25)
-
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(15, 28)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(18, 30)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(22, 32)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(25, 35)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 60 & defense.defense > 50:
-            return random.randint(25, 45)
-
-
-        # 70-61 Defense Team 2
-        elif offense.offense <= 20 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(13, 20)
-
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(13, 22)
-
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(15, 25)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(17, 30)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(20, 32)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(22, 35)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 70 & defense.defense > 60:
-            return random.randint(25, 40)
-
-
-        # 80-71 Defense
-        elif offense.offense <= 20 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(10, 20)
-
-        elif offense.offense <= 30 & offense.offense > 20 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(13, 22)
-
-        elif offense.offense <= 40 & offense.offense > 30 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(15, 22)
-
-        elif offense.offense <= 50 & offense.offense > 40 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(17, 25)
-
-        elif offense.offense <= 60 & offense.offense > 50 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(20, 28)
-
-        elif offense.offense <= 70 & offense.offense > 60 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(22, 30)
-
-        elif offense.offense <= 80 & offense.offense > 70 and defense.defense <= 80 & defense.defense > 70:
-            return random.randint(22, 30)
-
-        return 0
+        return get_random_value(offense.offense, defense.defense)
 
     @classmethod
-    def __get_goals(self, defense, sog):
-        goals = 0
-        for i in range(sog):
-            if defense.goalie <= 20:
-                if random.random() <= 0.115:
-                    goals += 1
+    def __get_goals(cls, sog, starting_goalie):
+        goalie_probs = {
+            20: 0.075,
+            30: 0.085,
+            40: 0.095,
+            50: 0.100,
+            60: 0.110,
+            70: 0.120,
+            80: 0.125
+        }
 
-            elif defense.goalie <= 30 & defense.goalie > 20:
-                if random.random() <= 0.110:
-                    goals += 1
+        def get_goal_probability(goalie_rating):
+            for threshold, prob in goalie_probs.items():
+                if goalie_rating <= threshold:
+                    return prob
+            return 0
 
-            elif defense.goalie <= 40 & defense.goalie > 30:
-                if random.random() <= 0.105:
-                    goals += 1
+        probability = get_goal_probability(starting_goalie)
+        # print(f"Starting goalie rating: {starting_goalie}, Probability: {probability}")
 
-            elif defense.goalie <= 50 & defense.goalie > 40:
-                if random.random() <= 0.102:
-                    goals += 1
-
-            elif defense.goalie <= 60 & defense.goalie > 50:
-                if random.random() <= 0.100:
-                    goals += 1
-
-            elif defense.goalie <= 70 & defense.goalie > 60:
-                if random.random() <= 0.090:
-                    goals += 1
-
-            elif defense.goalie <= 80 & defense.goalie > 70:
-                if random.random() <= 0.085:
-                    goals += 1
+        goals = sum(1 for _ in range(sog) if random.random() <= probability)
+        # print(f"Number of goals scored: {goals}")
 
         return goals
 
