457403c46a40171f3b2eb7f0cf9deca8b72cb39d
stevenpfaff <pfaffy1426@gmail.com>
2024-09-06 15:53:29 -0500

    redwings boost

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from team import teams
 from matchups import matchups
 
 if __name__ == "__main__":
-    num_simulations = 10000
+    num_simulations = 100000
     playoff_statistics = []
 
     for i in range(1, num_simulations + 1):
         simulator = SeasonSimulator()
         simulator.simulate_season(teams, matchups)
         simulator.sort_division_standings(filename="output/standings.csv", sim_number=i)
-        simulator.log_goalie_stats(sim_number=i)
+        # simulator.log_goalie_stats(sim_number=i)
         output_file = f"output/playoffs.csv"
         playoff_results = simulator.playoffs(output_file)
         playoff_statistics.extend(playoff_results)
