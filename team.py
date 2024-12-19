641d8a0a1dd08b0fa7d2b3a8862d13737aecc161
stevenpfaff <pfaffy1426@gmail.com>
2024-12-19 07:22:53 -0600

    roster updates

--- team.py
+++ team.py
@@ -28,7 +28,7 @@ class Team:
         self.conf_final = conf_final
         self.cup_final = cup_final
         self.cup_win = cup_win
-        self.max_selections = 42
+        self.max_selections = 40
         self.players = []
         self.reset_selections()  # Ensure goalie selections are initialized
 
